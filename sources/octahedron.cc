1963a588b6098d3701b191661b74e4879529f3fc
Yuji Tateno <webmaster@tarosuke.net>
2025-03-29 16:42:34 +0900

    正八面体への出力、正距円筒からの入力を記述

--- sources/octahedron.cc
+++ sources/octahedron.cc
@@ -1,5 +1,109 @@
+/*
+ * Copyright (C) 2025 tarosuke<webmaster@tarosuke.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#include <cmath>
+#include <tb/image.h>
+#include <tb/vector.h>
 
 
+
+struct In {
+	In(const tb::Image& i) : image(i) {};
+	virtual ~In() {};
+
+	// ベクタ(方向が指すテクスチャ上の色を返す)
+	virtual tb::Color GetColor(const tb::Vector<3, float>&) const = 0;
+
+protected:
+	const tb::Image& image;
+};
+
+struct Out {
+	Out(tb::Image& i, const In& in) : image(i), in(in) {
+		for (unsigned y(0); y < image.Height(); ++y) {
+			for (unsigned x(0); x < image.Width(); ++x) {
+				image[y][x] = GetColor(x, y);
+			}
+		}
+	};
+	virtual ~Out() {};
+
+protected:
+	tb::Image& image;
+	const In& in;
+
+	// テクスチャ座標から空間中の方向を求めてin.GetColor
+	tb::Color GetColor(unsigned x, unsigned y) const {
+		const float xx((float)x / image.Width() - 0.5);
+		const float yy((float)y / image.Height() - 0.5);
+		const float ax(std::fabs(xx));
+		const float ay(std::fabs(yy));
+		const float sx(std::signbit(xx));
+		const float sy(std::signbit(yy));
+
+		// 高さ(折り返し部分は負になる)
+		const float h(1.0f - ax - ay);
+
+		// 投影して正規化
+		if (h < 0) {
+			// 下側
+			tb::Vector<3, float> v{Fold(ax, sx), Fold(ay, sy), h};
+			v.Normalize();
+			return in.GetColor(v);
+		}
+
+		// 上側
+		tb::Vector<3, float> v{xx, yy, h};
+		v.Normalize();
+		return in.GetColor(v);
+	};
+
+private:
+	static float Fold(float a, float s) {
+		// 下側なので四隅を折り返す
+		return (1.0 - a) * s;
+	};
+};
+
+
+// 正距円筒
+struct Equirectangular : In {
+	Equirectangular(tb::Image& i) : In(i) {};
+
+private:
+	tb::Color GetColor(const tb::Vector<3, float>& v) const final {
+		const float e(std::atan2(v[2], std::sqrt(v[0] * v[0] + v[1] * v[1])));
+		const float d(std::atan2(v[1], v[0]));
+
+		return tb::Color(image.Get(d, e));
+	};
+};
+
+// スカイボックス
+struct Box : In {
+	Box(tb::Image& i) : In(i) {};
+
+private:
+	tb::Color GetColor(const tb::Vector<3, float>& v) const final {
+
+		return tb::Color();
+	};
+};
 
 
 int main() { return 0; }
